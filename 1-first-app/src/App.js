70ef866fbac8b0e4a14e4a6229381b0af7ef560e
Gedson Marcelino <gedson.marcelino@gmail.com>
2018-03-09 10:42:17 -0300

    - updated

--- 1-first-app/src/App.js
+++ 1-first-app/src/App.js
@@ -1,35 +1,45 @@
 import React, { Component } from 'react';
+import Radium from 'radium';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
   state = {
-    persons: {
-      name: 'Choper',
-      age: 10000
-    }
+    showItems: false ,
+    items: [
+      { name: 'Darth Vader', age: 200 },
+      { name: 'Shim', age: 25 },
+      { name: 'Passapartout', age: 38 }
+    ]
   }
 
-  switchNameHandler = () => {
-    console.log('Was clicked!');
-    this.setState({persons:{
-      name: 'New Choper',
-      age: 1
-    }});  
+  onToggleHandle = () => {
+    let value = !this.state.showItems;
+    this.setState({ showItems : value });
   }
 
   render() {
+    const style = {
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
+    }
+
     return (
       <div className="App">
         <h1>Hi, this is my first application using react.</h1>
-        <Person name="Darth Vader" age="200"/>
-        <Person name="Paspartour" age="35">Let's get traveled!</Person>
-        <Person name={this.state.persons.name} age={this.state.persons.age}/>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button style={style} onClick={this.onToggleHandle}>Show Items</button>
+        <hr/>
+        {this.state.showItems ?
+          <div>
+            {this.state.items.map(person => <Person name={person.name} age={person.age}/> )}
+          </div> : null
+        }
       </div>
     );
   }
 }
 
-export default App;
+export default Radium(App);
